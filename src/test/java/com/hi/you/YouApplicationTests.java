12c5e9898d6f773f8c7edb860667babd69a8b7a1
OnePuchPotato <zhibek.akylbekkyzy@alatoo.edu.kg>
2023-02-06 19:17:09 +0600

    first commit

---
+++ src/test/java/com/hi/you/YouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hi.you;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
